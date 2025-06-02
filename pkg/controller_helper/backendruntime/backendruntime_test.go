0d0c4102ccf2c33a61038c433af2b0709eb08804
X1aoZEOuO <nizefeng2002@outlook.com>
2025-06-02 11:12:16 +0800

    fix: fix test case structure in backend parser.
    Signed-off-by: X1aoZEOuO <nizefeng2002@outlook.com>

--- pkg/controller_helper/backendruntime/backendruntime_test.go
+++ pkg/controller_helper/backendruntime/backendruntime_test.go
@@ -21,6 +21,7 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 	coreapi "github.com/inftyai/llmaz/api/core/v1alpha1"
+	"github.com/inftyai/llmaz/test/util/wrapper"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -98,13 +99,14 @@ func TestRenderFlags(t *testing.T) {
 
 func TestBackendRuntimeParser_BasicFields(t *testing.T) {
 	type want struct {
-		cmd          []string
-		envs         []corev1.EnvVar
-		lifecycle    *corev1.Lifecycle
-		image        string
-		version      string
-		resourcesNil bool
-		shmNil       bool
+		cmd       []string
+		args      []string
+		envs      []corev1.EnvVar
+		lifecycle *corev1.Lifecycle
+		image     string
+		version   string
+		resources *inferenceapi.ResourceRequirements
+		shm       *resource.Quantity
 	}
 
 	testCases := []struct {
@@ -116,6 +118,7 @@ func TestBackendRuntimeParser_BasicFields(t *testing.T) {
 			name: "normal case has recommendConfig",
 			parser: func() *BackendRuntimeParser {
 				cmd := []string{"python", "serve.py"}
+				args := []string{"--port=8080"}
 				envs := []corev1.EnvVar{{Name: "MODE", Value: "release"}}
 				lc := &corev1.Lifecycle{
 					PostStart: &corev1.LifecycleHandler{
@@ -126,7 +129,7 @@ func TestBackendRuntimeParser_BasicFields(t *testing.T) {
 				res := &inferenceapi.ResourceRequirements{}
 
 				backend := &inferenceapi.BackendRuntime{
-					ObjectMeta: metav1.ObjectMeta{Name: "backend-1"},
+					ObjectMeta: metav1.ObjectMeta{Name: "default"},
 					Spec: inferenceapi.BackendRuntimeSpec{
 						Command:   cmd,
 						Envs:      envs,
@@ -135,8 +138,8 @@ func TestBackendRuntimeParser_BasicFields(t *testing.T) {
 						Version:   "v0.1.0",
 						RecommendedConfigs: []inferenceapi.RecommendedConfig{
 							{
-								Name:             "unit",
-								Args:             []string{},
+								Name:             "default",
+								Args:             args,
 								Resources:        res,
 								SharedMemorySize: shm,
 							},
@@ -144,72 +147,86 @@ func TestBackendRuntimeParser_BasicFields(t *testing.T) {
 					},
 				}
 
-				return &BackendRuntimeParser{
-					backendRuntime:      backend,
-					models:              []*coreapi.OpenModel{{}}, // 这里只需要占位
-					playground:          &inferenceapi.Playground{},
-					recommendConfigName: "unit",
-				}
+				models := []*coreapi.OpenModel{}
+				qwenModel := wrapper.MakeModel("qwen2-0--5b").FamilyName("qwen2").ModelSourceWithURI("ollama://qwen2:0.5b").Obj()
+				models = append(models, qwenModel)
+
+				playground := wrapper.MakePlayground("qwen2-0--5b", corev1.NamespaceDefault).ModelClaim("qwen2-0--5b").BackendRuntime("llmaz-ollama").BackendRuntimeEnv("OLLAMA_HOST", "0.0.0.0:8080").Replicas(1).Obj()
+
+				return NewBackendRuntimeParser(backend, models, playground)
 			}(),
 			want: want{
 				cmd:  []string{"python", "serve.py"},
+				args: []string{"--port=8080"},
 				envs: []corev1.EnvVar{{Name: "MODE", Value: "release"}},
 				lifecycle: &corev1.Lifecycle{
 					PostStart: &corev1.LifecycleHandler{
 						Exec: &corev1.ExecAction{Command: []string{"echo", "started"}},
 					},
 				},
-				image:        "inftyai/llama:v0.1.0",
-				version:      "v0.1.0",
-				resourcesNil: false,
-				shmNil:       false,
+				image:     "inftyai/llama:v0.1.0",
+				version:   "v0.1.0",
+				resources: &inferenceapi.ResourceRequirements{},
+				shm:       resource.NewQuantity(1*1024*1024*1024, resource.BinarySI),
 			},
 		},
 		{
 			name: "recommendConfigName not found and resources and SharedMemorySize return nil",
 			parser: func() *BackendRuntimeParser {
 				backend := &inferenceapi.BackendRuntime{
-					ObjectMeta: metav1.ObjectMeta{Name: "backend-2"},
+					ObjectMeta: metav1.ObjectMeta{Name: "default"},
 					Spec: inferenceapi.BackendRuntimeSpec{
 						Command: []string{"some"},
 						Image:   "repo/img",
 						Version: "latest",
+						RecommendedConfigs: []inferenceapi.RecommendedConfig{
+							{
+								Name: "default",
+							},
+						},
 					},
 				}
-				return &BackendRuntimeParser{
-					backendRuntime:      backend,
-					recommendConfigName: "not-found",
-				}
+
+				models := []*coreapi.OpenModel{}
+				qwenModel := wrapper.MakeModel("qwen2-0--5b").FamilyName("qwen2").ModelSourceWithURI("ollama://qwen2:0.5b").Obj()
+				models = append(models, qwenModel)
+
+				playground := wrapper.MakePlayground("qwen2-0--5b", corev1.NamespaceDefault).ModelClaim("qwen2-0--5b").BackendRuntime("llmaz-ollama").BackendRuntimeEnv("OLLAMA_HOST", "0.0.0.0:8080").Replicas(1).Obj()
+
+				return NewBackendRuntimeParser(backend, models, playground)
 			}(),
 			want: want{
-				cmd:          []string{"some"},
-				image:        "repo/img:latest",
-				version:      "latest",
-				resourcesNil: true,
-				shmNil:       true,
+				cmd:       []string{"some"},
+				image:     "repo/img:latest",
+				version:   "latest",
+				args:      []string{},
+				resources: nil,
+				shm:       nil,
 			},
 		},
 	}
 
 	for _, tc := range testCases {
-		tc := tc // capture
+		tc := tc // capture range variable
 		t.Run(tc.name, func(t *testing.T) {
 			p := tc.parser
-
 			if diff := cmp.Diff(tc.want.cmd, p.Command()); diff != "" {
 				t.Fatalf("Command() mismatch (-want +got):\n%s", diff)
 			}
-
-			if tc.want.envs != nil {
-				if diff := cmp.Diff(tc.want.envs, p.Envs()); diff != "" {
-					t.Fatalf("Envs() mismatch (-want +got):\n%s", diff)
-				}
+			args, err := p.Args()
+			if err != nil {
+				t.Fatal(err)
+			}
+			if diff := cmp.Diff(tc.want.args, args); diff != "" {
+				t.Fatalf("Args() mismatch (-want +got):\n%s", diff)
 			}
 
-			if tc.want.lifecycle != nil {
-				if diff := cmp.Diff(tc.want.lifecycle, p.Lifecycle()); diff != "" {
-					t.Fatalf("Lifecycle() mismatch (-want +got):\n%s", diff)
-				}
+			if diff := cmp.Diff(tc.want.envs, p.Envs()); diff != "" {
+				t.Fatalf("Envs() mismatch (-want +got):\n%s", diff)
+			}
+
+			if diff := cmp.Diff(tc.want.lifecycle, p.Lifecycle()); diff != "" {
+				t.Fatalf("Lifecycle() mismatch (-want +got):\n%s", diff)
 			}
 
 			if got := p.Image(p.Version()); got != tc.want.image {
@@ -220,12 +237,12 @@ func TestBackendRuntimeParser_BasicFields(t *testing.T) {
 				t.Fatalf("Version() = %s, want %s", got, tc.want.version)
 			}
 
-			if (p.Resources() == nil) != tc.want.resourcesNil {
-				t.Fatalf("Resources() nil? got %v, want %v", p.Resources() == nil, tc.want.resourcesNil)
+			if diff := cmp.Diff(tc.want.resources, p.Resources()); diff != "" {
+				t.Fatalf("Resources() mismatch (-want +got):\n%s", diff)
 			}
 
-			if (p.SharedMemorySize() == nil) != tc.want.shmNil {
-				t.Fatalf("SharedMemorySize() nil? got %v, want %v", p.SharedMemorySize() == nil, tc.want.shmNil)
+			if diff := cmp.Diff(tc.want.shm, p.SharedMemorySize()); diff != "" {
+				t.Fatalf("SharedMemorySize() mismatch (-want +got):\n%s", diff)
 			}
 		})
 	}
